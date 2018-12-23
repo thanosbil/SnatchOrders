7422e1ca5d61fb0a8dd6068e62e3c332f4968e6d
thanosbil <bilalis.ath@gmail.com>
2018-12-23 21:08:04 +0200

    Add project files.

---
+++ SnatchOrders/SnatchOrders/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SnatchOrders.Models;
+
+namespace SnatchOrders.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
