6ecc95344da55060a35ae97e6f6ca548d238d499
thanosbil <bilalis.ath@gmail.com>
2018-12-24 00:06:12 +0200

    Created Orders class

---
+++ SnatchOrders/SnatchOrders/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnatchOrders.Models
+{
+    /// <summary>
+    /// The main class with all the fields to populate the order email
+    /// </summary>
+    public class Order
+    {
+        public List<Item> Whiskey { get; set; }
+        public List<Item> Vodka { get; set; }
+        public List<Item> Rum { get; set; }
+        public List<Item> Tequila { get; set; }
+        public List<Item> Gin{ get; set; }
+        public List<Item> OtherLiquors { get; set; }
+    }
+}
