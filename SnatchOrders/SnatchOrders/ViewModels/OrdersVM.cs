6d967c08de3aae481a06eb8e4687cdc1fcc26ae6
thanosbil <bilalis.ath@gmail.com>
2018-12-27 19:10:01 +0200

    I commit to burgers

---
+++ SnatchOrders/SnatchOrders/ViewModels/OrdersVM.cs
@@ -0,0 +1,26 @@
+﻿using SnatchOrders.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SnatchOrders.ViewModels
+{
+    public class OrdersVM
+    {
+        public ICommand NewOrder { get; set; }
+        private INavigation _navigation;
+
+        public OrdersVM(INavigation navigation)
+        {
+            NewOrder = new Command(MakeNewOrder);
+            _navigation = navigation;
+        }
+
+        private async void MakeNewOrder()
+        {
+           await _navigation.PushAsync(new NewItemPage());
+        }
+    }
+}
--- SnatchOrders/SnatchOrders/Views/OrdersPage.xaml.cs
+++ SnatchOrders/SnatchOrders/Views/OrdersPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SnatchOrders.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,13 @@ namespace SnatchOrders.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class OrdersPage : ContentPage
 	{
+        OrdersVM ordersVM;
+
 		public OrdersPage ()
 		{
 			InitializeComponent ();
+            ordersVM = new OrdersVM(Navigation);
+            BindingContext = ordersVM;
 		}
 	}
 }
