be0588cf2bf1891642266e41e8df4c12094f1b9d
thanosbil <bilalis.ath@gmail.com>
2020-03-02 18:04:12 +0200

    Added new page  - ReportsPage

---
+++ SnatchOrders/SnatchOrders/Views/ReportsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SnatchOrders.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SnatchOrders.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ReportsPage : ContentPage {
+        ReportsPageViewModel reportsPageViewModel;
+        
+        public ReportsPage() {
+            InitializeComponent();
+            reportsPageViewModel = new ReportsPageViewModel(Navigation);
+            BindingContext = reportsPageViewModel;
+        }
+    }
+}
